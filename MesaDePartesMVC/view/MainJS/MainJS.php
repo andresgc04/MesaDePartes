15085a4e8b3f0510de5d1b5969af24099f4232d8
andresgc04 <andresgc1997@outlook.com>
2021-03-11 17:59:30 -0400

    MesaDePartesV4

---
+++ MesaDePartesMVC/view/MainJS/MainJS.php
@@ -0,0 +1,21 @@
+    <!-- Codebase Core JS -->
+    <script src="../../public/assets/js/core/jquery.min.js"></script>
+    <script src="../../public/assets/js/core/popper.min.js"></script>
+    <script src="../../public/assets/js/core/bootstrap.min.js"></script>
+    <script src="../../public/assets/js/core/jquery.slimscroll.min.js"></script>
+    <script src="../../public/assets/js/core/jquery.scrollLock.min.js"></script>
+    <script src="../../public/assets/js/core/jquery.appear.min.js"></script>
+    <script src="../../public/assets/js/core/jquery.countTo.min.js"></script>
+    <script src="../../public/assets/js/core/js.cookie.min.js"></script>
+    <script src="../../public/assets/js/codebase.js"></script>
+
+    <!-- Page JS Plugins -->
+    <!-- DataTable JS Plugins -->
+    <script src="../../public/assets/js/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="../../public/assets/js/plugins/datatables/dataTables.bootstrap4.min.js"></script>
+    <!-- End DataTables JS Plugins -->
+
+    <!-- Page JS Code -->
+    <!-- DataTables JS Code -->
+    <script src="../../public/assets/js/pages/be_tables_datatables.js"></script>
+    <!-- End DataTables JS Code -->
